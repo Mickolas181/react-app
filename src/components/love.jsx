bf2fe44bb0179535a5f15bfa51691d3c57e03b6f
Mickolas prathap sing K <mickolasprathapsing@gmail.com>
2026-03-07 21:32:15 +0530

    Update love.jsx

--- src/components/love.jsx
+++ src/components/love.jsx
@@ -2,7 +2,6 @@ export default function Love(){
     return <section className="py-60 bg-secondary"> 
         <div className=" flex justify-center font-bold text-black">
 
-            JAYA SELVI
 
         </div>
     </section>
